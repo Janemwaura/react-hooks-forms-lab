8d44d6faac00730feef8d5706f1649978366f45f
Janemwaura <mwaura461@gmail.com>
2023-12-06 21:38:36 +0300

    complete

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (event) => {
+    const newText = event.target.value;
+    setSearchText(newText);
+    onSearchChange(newText); // Pass the updated search text to the parent component
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={searchText} // Connect the input to state
+        onChange={handleSearchChange} // Set up an event listener to update state
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  // Initial state for the form data
+  const initialFormState = {
+    name: "",
+    category: "Produce",
+  };
+
+  // State to manage the form data
+  const [formData, setFormData] = useState(initialFormState);
+
+  // Event handler for input changes
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Event handler for form submission
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Create a new item object
+    const newItem = {
+      id: uuid(),
+      name: formData.name,
+      category: formData.category,
+    };
+
+    // Pass the new item to the parent component
+    onItemFormSubmit(newItem);
+
+    // Reset the form data to its initial state
+    setFormData(initialFormState);
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleInputChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
